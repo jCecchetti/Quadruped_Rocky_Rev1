77c96ec0d33525cb12f18635cde8857cacf5a944
Julia Cecchetti <julia.m.cecchetti@gmail.com>
2018-09-05 09:36:53 -0400

    Finished leg inverse kinematics

--- src/Leg.h
+++ src/Leg.h
@@ -8,27 +8,36 @@
 #ifndef LEG_H_
 #define LEG_H_
 
+class Position;
+
 class Leg {
 public:
 	Leg();
 	Leg(bool);
-	bool setFootPos();
+	bool setFootPos(Position);
 	virtual ~Leg();
 
 private:
 	bool calculateAngles(double, double, double);
 	//AdafruitServo servo1, servo2, servo3;
 
-	double hipAngle = 90;
-	double kneeAngle = 90;
-	double ankleAngle = 90;
-	float hipCenter = 90;
-	float kneeCenter = 90;
-	float ankleCenter = 7;
+	const double pi = 3.1416;
+
+	double hipAngle = pi/2.0;
+	double kneeAngle = pi/2.0;
+	double ankleAngle = pi/2.0;
+	float hipCenter = pi/2.0;
+	float kneeCenter = pi/2.0;
+	float ankleCenter = 7/180.0*pi;
 
 	float minimumPulseWidth = 0.6f;
 	float neutralPulseWidth = 1.575f;
 	float maximumPulseWidth = 2.55f;
+
+	double FEMUR = 1.6;
+	double TIBIA = 2.3585;
+	double TARSUS = 3.4;
+	double SERVOWIDTH = 1.927;
 
 	bool left = false;
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,146 +1,50 @@
-/**
- * Author: Jason White
- *
- * Description:
- * Reads joystick/gamepad events and displays them.
- *
- * Compile:
- * gcc joystick.c -o joystick
- *
- * Run:
- * ./joystick [/dev/input/jsX]
- *
- * See also:
- * https://www.kernel.org/doc/Documentation/input/joystick-api.txt
- */
-#include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
-#include </usr/include/linux/joystick.h>
+#include <time.h>
+#include <chrono>
+#include "Position.h"
+#include "Leg.h"
 
-/**
- * Reads a joystick event from the joystick device.
- *
- * Returns 0 on success. Otherwise -1 is returned.
- */
-int read_event(int fd, struct js_event *event)
-{
-    ssize_t bytes;
-
-    bytes = read(fd, event, sizeof(*event));
-
-    if (bytes == sizeof(*event))
-        return 0;
-
-    /* Error, could not read full event. */
-    return -1;
-}
-
-/**
- * Returns the number of axes on the controller or 0 if an error occurs.
- */
-size_t get_axis_count(int fd)
-{
-    __u8 axes;
-
-    if (ioctl(fd, JSIOCGAXES, &axes) == -1)
-        return 0;
-
-    return axes;
-}
-
-/**
- * Returns the number of buttons on the controller or 0 if an error occurs.
- */
-size_t get_button_count(int fd)
-{
-    __u8 buttons;
-    if (ioctl(fd, JSIOCGBUTTONS, &buttons) == -1)
-        return 0;
-
-    return buttons;
-}
-
-/**
- * Current state of an axis.
- */
-struct axis_state {
-    short x, y;
-};
-
-/**
- * Keeps track of the current axis state.
- *
- * NOTE: This function assumes that axes are numbered starting from 0, and that
- * the X axis is an even number, and the Y axis is an odd number. However, this
- * is usually a safe assumption.
- *
- * Returns the axis that the event indicated.
- */
-size_t get_axis_state(struct js_event *event, struct axis_state axes[3])
-{
-    size_t axis = event->number / 2;
-
-    if (axis < 3)
-    {
-        if (event->number % 2 == 0)
-            axes[axis].x = event->value;
-        else
-            axes[axis].y = event->value;
-    }
-
-    return axis;
-}
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const char *device;
-    int js;
-    struct js_event event;
-    struct axis_state axes[3] = {0};
-    size_t axis;
+	Position position(0,0,-4,0,0,0);
+	Leg leg(false);
+	leg.setFootPos(position);
 
-    if (argc > 1)
-        device = argv[1];
-    else
-        device = "/dev/input/js0";
+	//time timer = time();
+	/*double fps = 60;// updates per second
+	double timePerTick = 1000000000/fps;
+	double delta = 0;
+	long now;
+	long lastTime = 0;//nanotime
+	long timer = 0;
+	int ticks = 0;
+	long totalSeconds = 0;
+	bool running = true;*/
 
-    js = open(device, O_RDONLY);
+	/*
+	while(running){
+		now = std::chrono::system_clock::now();
+		delta += (now - lastTime) / timePerTick;
+		timer += now - lastTime;
+		lastTime = now;
+		//cout << now;
 
-    if (js == -1)
-        perror("Could not open joystick");
+		if(delta >= 1){
+			//update();
+			ticks++;
+			delta--;
+			//if(motion.end) running = false;
+		}
 
-    /* This loop will exit if the controller is unplugged. */
-    while (read_event(js, &event) == 0)
-    {
-        switch (event.type)
-        {
-            case JS_EVENT_BUTTON:
-                printf("Button %u %s\n", event.number, event.value ? "pressed" : "released");
-                break;
-            case JS_EVENT_AXIS:
-                axis = get_axis_state(&event, axes);
-                if (axis < 3)
-                    printf("Axis %u at (%6d, %6d)\n", axis, axes[axis].x, axes[axis].y);
-                break;
-            default:
-                /* Ignore init events. */
-                break;
-        }
-    }
+		if(timer >= 1000000000){
+			//System.out.println("Ticks and Frames " + ticks);
+			timer = 0;
+			ticks = 0;
+			totalSeconds++;
+		}
+	}*/
 
-    close(js);
-    return 0;
+	return 0;
 }
-
-/*
-int main(int argc, char *argv[])
-{
-   int fd = open ("/dev/input/js0", O_RDONLY);
-
-   struct js_event e;
-
-   read (fd, &e, sizeof(e));
-   close(fd);
-   return 0;
-}*/
